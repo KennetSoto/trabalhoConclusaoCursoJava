c9cb46e6810785019b3389daff83c31ee4587e35
kennet <kennet.soto@hotmail.com>
2023-08-22 21:38:08 -0300

    terceiro commit

--- src/main/java/br/com/crudComSpring/controler/ProdutoControler.java
+++ src/main/java/br/com/crudComSpring/controler/ProdutoControler.java
@@ -2,10 +2,15 @@ package br.com.crudComSpring.controler;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.crudComSpring.model.Produto;
 import br.com.crudComSpring.repository.ProdutoRepository;
@@ -41,8 +46,25 @@ public class ProdutoControler {
 
 	@GetMapping("/listagem_produtos")
 	public String listarProdutos(Model model) {
-		List<Produto> listaProdutos = produtoRepository.findAll();
-		model.addAttribute("lista_produtos", listaProdutos);
+		List<Produto> produto = produtoRepository.findAll();
+		model.addAttribute("lista_produtos", produto);
 		return "listagem_produtos";
 	}
+	
+	//get mappimg pega o endereço d pagina
+	@GetMapping("/cadastrarProduto")
+	public String paginaAdicionarProduto (Produto produto) {
+		return "adicionar_produtos";
+	}
+		
+	@PostMapping("/adicionar_produtos")
+	public String adicionarProduto (@Valid Produto produto,
+		Errors erros, BindingResult result, Model model) {
+		if (result.hasErrors() || (null != erros && erros.getErrorCount()> 0)) {
+			return "adicionar_produtos";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produtos";
+	}
+	
 }
